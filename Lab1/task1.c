0dfce17b6673b3cee6b127663f9468311244d8f3
Andrii <aabrasha@gmail.com>
2017-03-18 16:46:18 +0200

    - change code style to C99

--- Lab1/task1.c
+++ Lab1/task1.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,46 +24,57 @@ typedef struct _statistics statistics;
 
 int main(int argc, char *argv[]) {
 
+    char *inputFilename;
+    char *outputFilename;
+    char *line = NULL;
+    FILE *out;
+    FILE *in;
+
+    _IO_ssize_t count = 0;
+    size_t readCount = 0;
+
+    statistics *stats;
+
     if (argc < 3) {
         fprintf(stderr, "Too few arguments specified: %d", argc);
         exit(EXIT_FAILURE);
-    } else if (argc > 3){
+    } else if (argc > 3) {
         fprintf(stderr, "Too many arguments specified: %d", argc);
         exit(EXIT_FAILURE);
     }
 
     printf("Starting execution!\n");
 
-    char *inputFilename = argv[1];
-    FILE *in = openInputFile(inputFilename);
+    inputFilename = argv[1];
+    in = openInputFile(inputFilename);
     if (in == NULL) {
         fprintf(stderr, "Error reading input file: %s", inputFilename);
         exit(EXIT_FAILURE);
     }
 
-    char *outputFilename = argv[2];
-    FILE *out = openOutputFile(outputFilename);
+    outputFilename = argv[2];
+    out = openOutputFile(outputFilename);
     if (out == NULL) {
         fprintf(stderr, "Error opening / creating output file: %s", outputFilename);
     }
 
-    _IO_ssize_t count = 0;
-    size_t readCount = 0;
+    readCount = 0;
 
-    statistics *stats = malloc(sizeof(statistics));
+    stats = malloc(sizeof(statistics));
     stats->readLines = 0;
     stats->modifiedBits = 0;
 
-    char *line = NULL;
     while ((count = getline(&line, &readCount, in)) != -1) {
+        size_t s_len;
+        int i;
         printf("Read symbols: %lu | ", readCount);
         printf("Read count: %lu | ", count);
         printf("Read line: %s;", line);
 
         stats->readLines++;
 
-        size_t s_len = strlen(line);
-        for (int i = 0; i < s_len; ++i) {
+        s_len = strlen(line);
+        for (i = 0; i < s_len; ++i) {
             if (isalpha(line[i])) {
                 line[i] -= ASCII_SHIFT;
                 stats->modifiedBits++;
